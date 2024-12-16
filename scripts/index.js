de2873c04ce4e909c7c500290954e9fea18bd613
Joey Stempky <joey.stempky@gmail.com>
2024-12-16 11:02:54 -0500

    extract out content

--- scripts/index.js
+++ scripts/index.js
@@ -1,71 +1,13 @@
-const jsonContent = [
-	{
-		section: "education",
-		emoji: "🎓",
-		items: [
-			{
-				college: "Western Michigan University",
-				degree: "B.S.E. in Industrial & Entrepreneurial Engineering",
-				graduation: "December 2014"
-			},
-		]
-	},
-	{
-		section: "work",
-		emoji: "💼",
-		items: [
-			{
-				title: "Senior Consultant - Software Engineering",
-				company: "Inspire11",
-				duration: "July 2024 - Present",
-				location: "Chicago, IL (Remote)",
-			},
-			{
-				title: "Software Engineer",
-				company: "Integral",
-				duration: "March 2020 - July 2024",
-				location: "Royal Oak, MI (Remote)",
-			},
-		]
-	},
-	{
-		section: "skills",
-		emoji: "💻",
-		items: [
-
-			{
-				"backend languages": "JavaScript/TypeScript, Python, SQL",
-				tools: "React/React-Native, Node.js, Flask, Docker",
-				cloud: "AWS, GCP",
-			},
-		]
-	},
-	{
-		section: "projects",
-		emoji: "🚀",
-		items: [
-			{
-				name: "Portfolio",
-				description: "Personal website to showcase my work",
-				tech: "React, HTML, CSS",
-			},
-		]
-	},
-	{
-		section: "contact",
-		items: [
-			{
-				email: "test@email.com",
-			},
-		]
-	},
-
-]
-
 function renderContent() {
+	let jsonContent;
+	fetch("./scripts/content.json").then((res) => {
+		return res.json()
+	}).then((res) => {
+		jsonContent = res.jsonContent;
+	})
 	document.addEventListener('DOMContentLoaded', function() {
 		const mainEl = document.getElementById('main');
-		jsonContent.map((section) => {
+		jsonContent?.map((section) => {
 			const sectionEl = document.createElement('section');
 			sectionEl.id = section.section;
 			sectionEl.className = 'section';
